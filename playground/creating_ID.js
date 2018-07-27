e5952452a540bc5bcb583e5dc64629b6f0bd064f
Ash <akash.makam.am@gmail.com>
2018-07-28 01:45:05 +0530

    removed pg from repo

--- playground/creating_ID.js
+++
@@ -1,6 +0,0 @@
-// now we gon require MongoClient and ObjectID properties from mongodb the destructuring way!
-const {MongoClient, ObjectID} = require('mongodb');
-
-// lets create a new object _id 
-let myID = new ObjectID();
-console.log(myID);
