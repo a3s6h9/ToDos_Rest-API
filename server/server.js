80bde438402184ff135695516a852676a83c8772
Ash <akash.makam.am@gmail.com>
2018-07-29 17:53:51 +0530

    GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -36,6 +37,24 @@ app.get('/todos', (req, res) => {
       });
 
 });
+
+// GET todos /id
+app.get('/todos/:id', (req, res) => {
+ let id = req.params.id;;
+
+  if (!ObjectID.isValid(id)) {
+   return res.status(404).send();
+  }
+
+  Todo.findById(id)
+      .then( todo => {
+        if (!todo) {
+          res.status(404).send()
+        } else {
+          res.send({todo}) // same as todo: todo
+        }
+      }).catch( e => res.status(404).send())
+})
 
 app.listen(5000, () => {
   console.log(`Server started on Port 5000`);
