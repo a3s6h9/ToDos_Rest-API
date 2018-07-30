e90f3a98cd12b1b3315e779b93533dd032605efb
Ash <akash.makam.am@gmail.com>
2018-07-30 15:11:54 +0530

    DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+//'mongodb://huncho:imamakeit4real@ds023303.mlab.com:23303/todo_app'
+
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://huncho:imamakeit4real@ds023303.mlab.com:23303/todo_app' || 'mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true });
 
 module.exports = {
   mongoose
--- server/server.js
+++ server/server.js
@@ -58,6 +58,22 @@ app.get('/todos/:id', (req, res) => {
           res.send({todo}) // same as todo: todo
         }
       }).catch( e => res.status(404).send())
+});
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then( todo => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+      res.status(200).send(todo);
+      
+  }).catch( err => res.status(400).send());
 })
 
 app.listen(port, () => {
