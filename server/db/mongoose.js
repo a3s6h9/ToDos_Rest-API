73966153539156f36d837bd5cda8ad56fce2311d
Ash <akash.makam.am@gmail.com>
2018-07-31 01:41:48 +0530

    test0

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 //'mongodb://huncho:imamakeit4real@ds023303.mlab.com:23303/todo_app'
+//process.env.MONGODB_URI
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true });
+mongoose.connect( 'mongodb://huncho:imamakeit4real@ds023303.mlab.com:23303/todo_app'|| 'mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true });
 
 module.exports = {
   mongoose
