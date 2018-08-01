c322eba3d04082da279b018034e7d7fb077cc361
Ash <akash.makam.am@gmail.com>
2018-08-02 00:48:13 +0530

    hash users pass && mongoose pre

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 const UserSchema = new mongoose.Schema({
@@ -76,6 +77,21 @@ UserSchema.statics.findByToken = function(token) {
   });
 
 }
+
+UserSchema.pre('save', function(next) {
+  let user= this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+})
 
 let User = mongoose.model('User', UserSchema); // we cant define methods in a model so we need to create a mongoose Shema.
 
